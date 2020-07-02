a4999d9bc1f962c165f4c762a3c356dbaa003ebd
Liwen <liwen11338@gmail.com.tw>
2020-07-02 14:26:49 +0800

    新增，年齡改成生日。修改，可以顯示病友的資料，日期的部分按下按鈕會顯示出生日日期或註冊日期，性別的按鈕會預設並友性別。

--- app/src/main/java/com/example/newlogin/MainActivity.java
+++ app/src/main/java/com/example/newlogin/MainActivity.java
@@ -2,23 +2,24 @@ package com.example.newlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     Intent intent = new Intent();
-    EditText editText,Account;
+    EditText editText, Account;
     ImageButton button;
     boolean canSee;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,34 +27,95 @@ public class MainActivity extends AppCompatActivity {
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
         //getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        Account= (EditText) findViewById(R.id.editText);
-        editText= (EditText) findViewById(R.id.editText2);
-        button= (ImageButton) findViewById(R.id.change);
+        db = openOrCreateDatabase("DBS", Context.MODE_PRIVATE, null);
+        //db.setForeignKeyConstraintsEnabled(true);
+        createNurseTable();
+        createPatientTable();
+        createTopicTable();
+        createStudyTable();
+        createQuestionTable();
+        createExamTable();
+        createAnswerTable();
+        Account = (EditText) findViewById(R.id.edt_search);
+        editText = (EditText) findViewById(R.id.editText2);
+        button = (ImageButton) findViewById(R.id.change);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //通过全局的一个变量的设置，这个就是判断控件里面的内容是不是能被看到
-                if (canSee==false){
+                if (canSee == false) {
                     //如果是不能看到密码的情况下，
                     editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                    canSee=true;
-                }else {
+                    canSee = true;
+                } else {
                     //如果是能看到密码的状态下
                     editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                    canSee=false;
+                    canSee = false;
                 }
             }
         });
     }
-    public void choicepatient(View v){
+
+    public void choicepatient(View v) {
         intent.setClass(this, Searchlogin.class);
-        intent.putExtra("name", Account.getText().toString());
+       // intent.putExtra("name", Account.getText().toString());
         startActivity(intent);
         finish();
     }
-    public  void  back(View v){
-        Intent i=new Intent(MainActivity.this,Backstage_main.class);
+
+    public void back(View v) {
+        Intent i = new Intent(MainActivity.this, Backstage_main.class);
         startActivity(i);
         finish();
+    }
+
+    private void createNurseTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Nurse (nurse_id TEXT NOT NULL, nurse_name TEXT NOT NULL, nurse_password TEXT NOT NULL, nurse_authority INT NOT NULL, PRIMARY KEY(nurse_id))";
+        db.execSQL(sql);
+        ContentValues contentValues = new ContentValues(1);
+        Cursor cursor = db.rawQuery("SELECT * FROM Nurse", null);
+        if (!cursor.moveToFirst()) {
+            contentValues.put("nurse_id", "admin");
+            contentValues.put("nurse_name", "Admin");
+            contentValues.put("nurse_password", "admin");
+            contentValues.put("nurse_authority", 1);
+            db.insert("Nurse", null, contentValues);
+
+        }
+    }
+
+    private void createPatientTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Patient (patient_id TEXT NOT NULL, patient_name TEXT NOT NULL, patient_gender INT, patient_register DATE, patient_sign TEXT, patient_birth DATE , patient_incharge TEXT NOT NULL, PRIMARY KEY(patient_id), FOREIGN KEY(patient_incharge) REFERENCES Nurse(nurse_id) ON DELETE SET NULL ON UPDATE CASCADE)";
+        db.execSQL(sql);
+        db.execSQL("PRAGMA foreign_keys=ON;");
+    }
+
+    private void createTopicTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Topic (topic_id INT, topic_name TEXT, PRIMARY KEY(topic_id))";
+        db.execSQL(sql);
+    }
+
+    private void createStudyTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Study (study_id INT, study_date DateTime, topic_id INT, patient_id TEXT, nurse_id TEXT, PRIMARY KEY(study_id), FOREIGN KEY (topic_id) REFERENCES Topic(topic_id) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY (patient_id) REFERENCES Patient(patient_id) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY (nurse_id) REFERENCES Nurse(nurse_id) ON DELETE SET NULL ON UPDATE CASCADE)";
+        db.execSQL(sql);
+        db.execSQL("PRAGMA foreign_keys=ON;");
+    }
+
+    private void createQuestionTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Question (question_id INT, question_content TEXT, question_answer INT, question_s1 CHAR(12), question_s2 CHAR(12), question_s3 CHAR(12), question_s4 CHAR(12), question_explain CHAR(40), exam_id INT, topic_id INT, PRIMARY KEY(question_id), FOREIGN KEY(exam_id) REFERENCES Exam(exam_id) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY(topic_id) REFERENCES Topic(topic_id) ON DELETE SET NULL ON UPDATE CASCADE)";
+        db.execSQL(sql);
+        db.execSQL("PRAGMA foreign_keys=ON;");
+    }
+
+    private void createExamTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Exam (exam_id INT, exam_date DateTime, exam_score INT,question_id INT, patient_id CHAR(10), nurse_id CHAR(10), PRIMARY KEY(exam_id), FOREIGN KEY(patient_id) REFERENCES Patient(patient_id) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY(nurse_id) REFERENCES Nurse(nurse_id) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY(question_id) REFERENCES Question(question_id) ON DELETE SET NULL ON UPDATE CASCADE)";
+        db.execSQL(sql);
+        db.execSQL("PRAGMA foreign_keys=ON;");
+    }
+
+    private void createAnswerTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Answer (result INT, patient_answer INT, question_id INT, exam_id INT, PRIMARY KEY(exam_id, question_id), FOREIGN KEY(exam_id) REFERENCES Exam(exam_id)ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY(question_id) REFERENCES Question(question_id) ON DELETE SET NULL ON UPDATE CASCADE)";
+        db.execSQL(sql);
+        db.execSQL("PRAGMA foreign_keys=ON;");
     }
 }
