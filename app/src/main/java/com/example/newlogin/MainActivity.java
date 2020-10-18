87271e08da0f5acce6fc3311e4c5eaa2f25b4f0e
Liwen <liwen11338@gmail.com.tw>
2020-10-18 20:07:43 +0800

    變動問題資 料庫

--- app/src/main/java/com/example/newlogin/MainActivity.java
+++ app/src/main/java/com/example/newlogin/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
           });
     }
 
-    private void insertQuestion(String question_id,String content,String s1,String s2,String s3,String s4,String explain, String topic_id,String An)
+    private void insertQuestion(String question_id,String content,String s1,String s2,String explain, String topic_id,String An)
     {
         ContentValues cv =new ContentValues(1);//10
         cv.put("question_id",question_id);
@@ -82,8 +82,6 @@ public class MainActivity extends AppCompatActivity {
         cv.put("question_answer",An);
         cv.put("question_s1",s1);
         cv.put("question_s2",s2);
-        cv.put("question_s3",s3);
-        cv.put("question_s4",s4);
         cv.put("question_explain",explain);
         cv.put("topic_id",topic_id);
         db.insert("Question", null, cv);
@@ -360,16 +358,19 @@ public class MainActivity extends AppCompatActivity {
         ContentValues contentValues = new ContentValues(1);
         Cursor cursor = db.rawQuery("SELECT * FROM Topic", null);
         if (!cursor.moveToFirst()) {
-            insertTopic("t1","壹.腎臟功能簡介.doc.pdf");//這個之後要改成t1
-            insertTopic("t2","貳.甚麼是慢性腎臟病.doc.pdf");
-            insertTopic("t3","參.健康人如何保護自己健康.doc.pdf");
-            insertTopic("t4","肆.腎衰竭的原因.doc.pdf");
-            insertTopic("t5","伍.腎衰竭原因治療.doc.pdf");
-            insertTopic("t6","陸.血液透析治療與照護.doc.pdf");
-            insertTopic("t7","柒.何為血液透析.doc.pdf");
-            insertTopic("t8","捌.動靜脈廔管的照顧.doc.pdf");
-            insertTopic("t9","玖.腎友如何預防便祕.doc.pdf");
-            insertTopic("t10","拾..doc.pdf");
+            insertTopic("t1","壹.腎臟功能簡介.pdf");//這個之後要改成t1
+            insertTopic("t2","貳.甚麼是慢性腎臟病.pdf");
+            insertTopic("t3","參.健康人如何保護自己健康.pdf");
+            insertTopic("t4","肆.腎衰竭的原因.pdf");
+            insertTopic("t5","伍.腎衰竭原因治療.pdf");
+            insertTopic("t6","陸.血液透析治療與照護.pdf");
+            insertTopic("t7","柒.何為血液透析.pdf");
+            insertTopic("t8","捌.動靜脈廔管的照顧.pdf");
+            insertTopic("t9","玖.腎友如何預防便祕.pdf");
+            insertTopic("t10","拾.甚有皮膚搔癢該如何處理.pdf");
+            insertTopic("t11","拾壹.乾體重.pdf");
+            insertTopic("t12","拾貳.血管通路的介紹.pdf");
+            insertTopic("t13","拾參.飲食控制要點.pdf");
         }
     }
 
@@ -383,55 +384,57 @@ public class MainActivity extends AppCompatActivity {
     private void createQuestionTable()
     {
         //String question_id,String content,String s1,String s2,String s3,String s4,String explain, String topic_id,String An
-        String sql = "CREATE TABLE IF NOT EXISTS Question (question_id TEXT, question_content TEXT, question_answer TEXT, question_s1 CHAR(12), question_s2 CHAR(12), question_s3 CHAR(12), question_s4 CHAR(12), question_explain CHAR(40), exam_id TEXT, topic_id TEXT,change_data INT,  PRIMARY KEY(question_id), FOREIGN KEY(exam_id) REFERENCES Exam(exam_id) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY(topic_id) REFERENCES Topic(topic_id) ON DELETE SET NULL ON UPDATE CASCADE)";
+        String sql = "CREATE TABLE IF NOT EXISTS Question (question_id TEXT, question_content TEXT, question_answer TEXT, question_s1 TEXT, question_s2 TEXT, question_explain TEXT, exam_id TEXT, topic_id TEXT,change_data INT,  PRIMARY KEY(question_id), FOREIGN KEY(exam_id) REFERENCES Exam(exam_id) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY(topic_id) REFERENCES Topic(topic_id) ON DELETE SET NULL ON UPDATE CASCADE)";
         db.execSQL(sql);
         db.execSQL("PRAGMA foreign_keys=ON;");
         Cursor cursor = db.rawQuery("SELECT * FROM Question", null);
         if (!cursor.moveToFirst()) {
-            String content="血液透析急性併發征不包括：";
-            String s1="發熱";
-            String s2="肌肉痙攣";
-            String s3="失衡綜合征";
-            String s4="透析性骨病";
-            String An1="透析性骨病";
-            String explain="急性并发症分為：透析失衡综合征：主要发生于肌酐、尿素氮等毒素偏高明显患者。主要症状有恶心、呕吐、头痛、疲乏、烦躁不安等，严重者可有抽搐、震颤。首次使用综合征：主要是应用新透析器及管道所引起的。多发生在透析开始后几分钟至1小时左右，表现为呼吸困难，全身发热感，可突然心跳骤停。血栓的形成，有可能是因为抗凝不到位、长时间卧床，置管、管路、滤器等异物刺激血小板聚集。还可以发生透析中低血压、高血压、头痛、肌肉痉挛、心律失常等。";
-            insertQuestion( "1", content, s1, s2, s3, s4, explain, "t1", An1);
+            //t1的部分
+            insertQuestion( "1", "腎臟是一對位於後腹腔的器官,右腎較左腎略低。", "1", "0",  "", "t1", "1");
+            insertQuestion( "2", "腎臟是人體主要的排毒器官，負責清除血液中的代謝廢物。", "1", "0",   "", "t1", "1");
+            insertQuestion( "3", "腎臟所分泌的腎素（Renin），為調節血壓維持恆定重要的荷爾蒙。", "1", "0",   "", "t1", "1");
+            insertQuestion( "4", "每個人每天約有3,000毫升（ML）的尿液。", "1", "0","每天約有1,500～2,000毫升（ML）的尿液。", "t1", "0");
+            insertQuestion( "5", "體內水份過多排不出時，血壓也可能會升高。", "1", "0","", "t1", "1");
+            insertQuestion( "6", "當腎臟功能喪失，會導致體內毒素無法排除，造成尿毒症。", "1","", "", "t1", "1");
+            insertQuestion( "7", "腎絲球廓清率下降至每分鐘30～35毫升時，稱做末期腎衰竭(尿毒)。", "1", "0", "是屬於中度慢性腎臟病，如控制不好，離末期腎衰竭也不遠了。", "t1", "0");
+            insertQuestion( "8", "腎絲球廓清率下降至每分鐘5～15毫升時，稱做末期腎衰竭(尿毒)。", "1", "0",  "", "t1", "1");
+            insertQuestion( "9", "腎臟功能喪失，鉀離子排泄下降，嚴重時會導致死亡。", "1", "0", "", "t1", "1");
+            insertQuestion( "10", "腎臟功能喪失，不會造成貧血。", "1", "0", "腎臟功能下降時，會影響血球的功能，且會影響紅血球生素的製造，故會產生貧血。", "t1", "0");
+            insertQuestion( "11", "腎臟功能不良時，初期容易感到疲倦、倦怠、食慾不振、失眠等症狀。", "1", "0", "", "t1", "1");
 
-            content="血液透析室應當根據設備要求定期對水處理系統進行沖洗消毒，並定期進行水質檢測。每次沖洗消毒後均應_____，確保安全。";
-            s1="監測水中細菌量";
-            s2="測定管路中消毒液殘留量";
-            s3="測定管路壓力";
-            s4="不需要測定任何專案";
-            An1="測定管路中消毒液殘留量";
-            explain="急性并发症分為：透析失衡综合征：主要发生于肌酐、尿素氮等毒素偏高明显患者。主要症状有恶心、呕吐、头痛、疲乏、烦躁不安等，严重者可有抽搐、震颤。首次使用综合征：主要是应用新透析器及管道所引起的。多发生在透析开始后几分钟至1小时左右，表现为呼吸困难，全身发热感，可突然心跳骤停。血栓的形成，有可能是因为抗凝不到位、长时间卧床，置管、管路、滤器等异物刺激血小板聚集。还可以发生透析中低血压、高血压、头痛、肌肉痉挛、心律失常等。";
-            insertQuestion( "2", content, s1, s2, s3, s4, explain, "t1", An1);
+            //t2的部分
+            insertQuestion( "12", "腎臟受損超過三個月，結構或功能無法恢復正常時，稱為「急性腎損傷」。", "1", "0", "為慢性腎臟病才對。", "t2", "0");
+            insertQuestion( "13", "腎臟受損超過三個月，結構或功能無法恢復正常時，稱為「慢性」腎臟病。", "1", "0", "", "t2", "1");
+            insertQuestion( "14", "一旦發生慢性腎臟病，現行醫學並無法恢復腎功能，只能減緩腎臟功能衰退的速度。", "1", "0", "", "t2", "1");
+            insertQuestion( "15", "急性腎損傷經過適當的治療，大部分可使腎功能恢復正常。", "1", "0", "", "t2", "1");
+            insertQuestion( "16", "腎臟病徵兆的口訣：泡、水、高、貧、倦。", "1", "0", "", "t2", "1");
+            insertQuestion( "17", "當腎絲球過濾率衰退至每分鐘30-59毫升，就需要開始透析治療。", "1", "0", "腎絲球過濾率小於每分鐘15毫升，才需依臨床症狀決定是否立即透析治療。", "t2", "0");
+            insertQuestion( "18", "慢性腎衰竭，小便常可以看到泡泡尿。", "1", "0", "", "t2", "1");
+            insertQuestion( "19", "當看到小便出現許多泡泡，可能是因為解尿衝擊造成的的水波，沒有關係。", "1", "0", "", "t2", "0");
+            insertQuestion( "20", "慢性腎衰竭常發生高血鉀，但不會致死。", "1", "0", "高血鉀會產生心律不整，嚴重會致死。", "t2", "0");
+            insertQuestion( "21", "腎衰竭病人口腔有金屬味或尿味，主要是牙周病。", "1", "0", "是體內含氮廢物未能排除所導致的味道。", "t2", "0");
+            insertQuestion( "22", "腎衰竭病人水腫常見於下肢與腳踝水腫以及晨間眼部浮腫。", "1", "0", "", "t2", "1");
 
-            content="血液透析室應當建立嚴格的接診制度，對所有初次透析的患者進行乙型肝炎、病毒、丙型肝炎病毒、梅毒、愛滋病病毒感染的相關檢查，每_____複查1次。";
-            s1="月";
-            s2="季度";
-            s3="半年";
-            s4="年";
-            An1="半年";
-            explain="急性并发症分為：透析失衡综合征：主要发生于肌酐、尿素氮等毒素偏高明显患者。主要症状有恶心、呕吐、头痛、疲乏、烦躁不安等，严重者可有抽搐、震颤。首次使用综合征：主要是应用新透析器及管道所引起的。多发生在透析开始后几分钟至1小时左右，表现为呼吸困难，全身发热感，可突然心跳骤停。血栓的形成，有可能是因为抗凝不到位、长时间卧床，置管、管路、滤器等异物刺激血小板聚集。还可以发生透析中低血压、高血压、头痛、肌肉痉挛、心律失常等。";
-            insertQuestion( "3", content, s1, s2, s3, s4, explain, "t1", An1);
+            //t3的部分
+            insertQuestion( "23", "有病治病，無病強身，所以補品和健康食品都可以多吃無害。", "1", "0", "補品及健康食品吃過多時，仍可能造成腎臟負擔，且許多的添加也可能會導致腎臟傷害。", "t3", "0");
+            insertQuestion( "24", "補藥、地攤藥、「健康食品」、減肥藥、止痛劑、類固醇、抗生素及不明來歷的藥品等，這些都可能傷害我們的腎臟。", "1", "0", "", "t3", "1");
+            insertQuestion( "25", "美國糖尿病學會建議三餐前全血血糖控制在80-120毫克/毫升。", "1", "0", "", "t3", "1");
+            insertQuestion( "26", "即使大吃大喝，只要每天都有在運動，並不會影響身體健康。", "1", "0", "過度大吃大喝本來就對身體造成負荷影響健康。", "t3", "0");
+            insertQuestion( "27", "當收縮壓常態性超過140毫米汞柱時，代表已經有輕度的高血壓跡象。", "1", "0", "", "t3", "1");
+            insertQuestion( "28", "高血壓前期，還不至於高血壓，所以可以不管他。", "1", "0", "如未積極介入預防，會加快高血壓的進程。", "t3", "0");
+            insertQuestion( "29", "腎臟保養之道應該要生活起居正常、避免腎毒性物質（如止痛劑、顯影劑、消炎藥）、攝取充足的水份、多運動等。", "1", "0", "", "t3", "1");
+            insertQuestion( "30", "因為時常會頭痛，所以固定吃止痛藥沒關係。", "1", "0", "長期服用止痛藥，就可能會造成腎臟病變。", "t3", "0");
+            insertQuestion( "31", "常態性收縮壓在120-139，舒張壓80-89毫米汞柱，代表有高血壓前期的癥症。", "1", "0", "", "t3", "1");
+            insertQuestion( "32", "適當的運動有助於血壓及血糖的控制。", "1", "0", "", "t3", "1");
 
-            content="血液透析室使用的消毒藥械、一次性醫療器械和器具應當符合國家有關規定。一次性使用的醫療器械、器具_____。";
-            s1="不得重複使用";
-            s2="可以重複使用";
-            s3="部分貴重的可以重複使用，但必須進行嚴格的消毒滅菌";
-            s4="應進行可回收利用";
-            An1="部分貴重的可以重複使用，但必須進行嚴格的消毒滅菌";
-            explain="急性并发症分為：透析失衡综合征：主要发生于肌酐、尿素氮等毒素偏高明显患者。主要症状有恶心、呕吐、头痛、疲乏、烦躁不安等，严重者可有抽搐、震颤。首次使用综合征：主要是应用新透析器及管道所引起的。多发生在透析开始后几分钟至1小时左右，表现为呼吸困难，全身发热感，可突然心跳骤停。血栓的形成，有可能是因为抗凝不到位、长时间卧床，置管、管路、滤器等异物刺激血小板聚集。还可以发生透析中低血压、高血压、头痛、肌肉痉挛、心律失常等。";
-            insertQuestion( "4", content, s1, s2, s3, s4, explain, "t1", An1);
-
-            content="血液透析複用用水的常規內毒素檢測應_____\"+\"\\n\"+\"至少一次。";
-            s1="不得重複使用";
-            s2="可以重複使用";
-            s3="部分貴重的可以重複使用，但必須進行嚴格的消毒滅菌";
-            s4="每季";
-            An1="部分貴重的可以重複使用，但必須進行嚴格的消毒滅菌";
-            explain="急性并发症分為：透析失衡综合征：主要发生于肌酐、尿素氮等毒素偏高明显患者。主要症状有恶心、呕吐、头痛、疲乏、烦躁不安等，严重者可有抽搐、震颤。首次使用综合征：主要是应用新透析器及管道所引起的。多发生在透析开始后几分钟至1小时左右，表现为呼吸困难，全身发热感，可突然心跳骤停。血栓的形成，有可能是因为抗凝不到位、长时间卧床，置管、管路、滤器等异物刺激血小板聚集。还可以发生透析中低血压、高血压、头痛、肌肉痉挛、心律失常等。";
-            insertQuestion( "5", content, s1, s2, s3, s4, explain, "t1", An1);
+            //t4的部分
+            insertQuestion( "33", "糖尿病腎病變已變為末期腎衰竭之首要原因。", "1", "0", "", "t4", "1");
+            insertQuestion( "34", "血壓控制不良，也會造成腎臟功能惡化。", "1", "0", "", "t4", "1");
+            insertQuestion( "35", "抽菸會增加致癌的風險，也會對腎臟的血管造成傷害。", "1", "0", "", "t4", "1");
+            insertQuestion( "36", "抽菸主要傷害肺部，不會對腎臟血管有傷害。", "1", "0", "吸菸會影響腎臟動脈的變化，使腎臟血管收縮及血流速度減少，刺激血管收縮的功能，使血壓上昇，而且也會增加蛋白尿的情形加重，造成腎功能惡。", "t4", "0");
+            insertQuestion( "37", "腎臟功能隨著老化的過程逐年衰退，所以要小心愛護。", "1", "0", "", "t4", "1");
+            insertQuestion( "38", "老化也會讓腎臟功能下降。", "1", "0", "", "t4", "1");
+            insertQuestion( "38", "老化也會讓腎臟功能下降。", "1", "0", "", "t4", "1");
         }
     }
 
